39a345060c5a21af348f3c4237d3355da05dccc6
Karlson but on Laptop <23259@wc.school.nz>
2026-03-12 09:19:37 +1300

    added my first bit of real code

--- Assesment.py
+++ Assesment.py
@@ -22,7 +22,7 @@ class Weapon:
         pass
 
     def pick_up(self):
-        print()
+        print(self.pickup_message)
 
 
 class Enemy:
@@ -55,7 +55,7 @@ weapons = []
 weapon_data = [
     ("AK-47",40,30,70,650,"assault",500,"You pick up the AK-47, Chambered in 7.62. It feels heavy in your hands. Maybe too heavy"),
     ("AK-12",34,30,80,750,"assault",700,"You grab the AK-12. You know this gun. It's like the '47', but more balanced. In theory"),
-    ("Fal 50.0",49,20,75,400,"assault",1000),
+    ("Fal 50.0",49,20,75,400,"assault",1000,"You pick up the FAl 50.0. It takes 7.62x51mm Nato rounds. A Beast, hopefully"),
     ("AUG A3",32,30,90,850,"assault",1400),
     ("P90",24,50,60,1100,"smg",1200),
     ("G11",15,33,65,2100,"assault",2600),
