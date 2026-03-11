63a107abc2fcd0773ec3861aea1f7629a1890d9c
Karlson but on Laptop <23259@wc.school.nz>
2026-03-11 22:19:52 +1300

    Testing from my laptop

--- Assesment.py
+++ Assesment.py
@@ -34,3 +34,9 @@ class Item:
     def __init__(self,name,value):
         self.name = name
         self.value = value
+
+
+
+
+def main():
+    print("Welcome to the game of something hopefully cool")
