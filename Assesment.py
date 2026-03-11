72cb6c1b45c1f63af3ea5e5ad8ce0dc331877e3b
Karlson but on Laptop <23259@wc.school.nz>
2026-03-11 23:01:02 +1300

    added my first bit of real code

--- Assesment.py
+++ Assesment.py
@@ -1,4 +1,4 @@
-
+import time
 
 class Player:
     def __init__(self, name, health,inv):
@@ -8,16 +8,21 @@ class Player:
 
 
 class Weapon:
-    def __init__(self, name,damage,clip_cap,accuracy,fire_rate):
+    def __init__(self, name,damage,clip_cap,accuracy,fire_rate,bullet,value,pickup_message):
         self.name = name
         self.damage = damage
         self.clip_cap = clip_cap
         self.accuracy = accuracy
         self.fire_rate = fire_rate
+        self.bullet = bullet
+        self.value = value
+        self.pickup_message = pickup_message
 
     def shoot(self):
         pass
 
+    def pick_up(self):
+        print()
 
 
 class Enemy:
@@ -37,6 +42,35 @@ class Item:
 
 
 
+def slow_text(text):
+
+    for i in text:
+        print(text[i])
+        time.sleep(0.03)
+
+
+
+weapons = []
+
+weapon_data = [
+    ("AK-47",40,30,70,650,"assault",500,"You pick up the AK-47, Chambered in 7.62. It feels heavy in your hands. Maybe too heavy"),
+    ("AK-12",34,30,80,750,"assault",700,"You grab the AK-12. You know this gun. It's like the '47', but more balanced. In theory"),
+    ("Fal 50.0",49,20,75,400,"assault",1000),
+    ("AUG A3",32,30,90,850,"assault",1400),
+    ("P90",24,50,60,1100,"smg",1200),
+    ("G11",15,33,65,2100,"assault",2600),
+
+
+]
+
+for i in weapon_data:
+
+    weapons.append(Weapon(*i))
+
+
 
 def main():
     print("Welcome to the game of something hopefully cool")
+
+
+
