630cebde57d268c1f378bfddc4f32117d50fe14d
Qaun on lynux <23259@wc.school.nz>
2026-03-15 01:22:34 +1300

    Made the enemy drop stuff when killed

--- Assesment.py
+++ Assesment.py
@@ -1,280 +1,308 @@
 import random
 import time
 
+while True:
+
+    try:
+
+
+        def battle(do_you_shoot_first,name):
+
+
+            #Sets the enemy for ease of use. will change to be random based on things later
+            enemy = Enemy(name,random.randint(600,1400),[all_weapons[0]])
+
+
+            #just loop intull someone dies
+            while player.health>0 and enemy.health > 0:
+
+                #only runs the first time if you can shoot first
+                if do_you_shoot_first:
+
+
+                    print("What weapon do you want to use:")
+
+
+                    #list all the weapons that you have and let you pick one
+
+                    for i in range(len(player.inv)):
+
+                        print(f"{i+1}: ", player.inv[i].name)
+
+                    choice = int(input("> "))
+
+                    player_weapon = player.inv[choice-1]
+
+
+                    #if choice == 1:
+
+
+                    #Sets the damage and bullets with the class func
+                    damage, bullets = player_weapon.shoot()
+
+                    enemy.health -= damage
+
+                    #Sets things health to zero if its negative for nicer visuals
+                    if enemy.health < 0:
+                        enemy.health = 0
+
+
+                    print(f"You shot {bullets} rounds for a total of {damage} damage")
+                    print(f"The {enemy.name} now has {enemy.health} health remaining")
+                    print()
+
+
+                do_you_shoot_first = True
+
+                if enemy.health <= 0:
+                    break
+
+
+                #picks a random weapon out of the enemys inventory
+                op_weapon = random.choice(enemy.weapons)
+
+                #just does the same thing as before but for the enemy.
 
 
 
-def battle(do_you_shoot_first,name):
+                damage, bullets = op_weapon.shoot()
+
+                player.health -= damage
+
+                if player.health <0:
+                    player.health = 0
+
+                print(f"The {enemy.name} shot {bullets} rounds into you for a total of {damage} damage")
+                print(f"You are now on {player.health} health")
+                print()
 
 
-    #Sets the enemy for ease of use. will change to be random based on things later
-    enemy = Enemy(name,random.randint(600,1400),[all_weapons[0]])
+            if player.health == 0:
+                print("GAME OVER")
+
+            #A 30 percent chance for the enemy to drop something random out of their inventory
+            item_drop = 0
+
+            if random.randint(1,10) > 7:
+                item_drop = random.choice(enemy.weapons)
 
 
-    #just loop intull someone dies
-    while player.health>0 and enemy.health > 0:
+            print("You won the fight!!")
 
-        #only runs the first time if you can shoot first
-        if do_you_shoot_first:
+            if item_drop:
+                print(f"The {enemy.name} dropped a {item_drop.name}")
+                player.inv.append(item_drop)
 
-
-            print("What weapon do you want to use:")
-
-
-            #list all the weapons that you have and let you pick one
-
-            for i in range(len(player.inv)):
-
-                print(f"{i+1}: ", player.inv[i].name)
-
-            choice = int(input("> "))
-
-            player_weapon = player.inv[choice-1]
+            else:
+                print(f"The {enemy.name} didn't drop anything")
 
 
-            #if choice == 1:
 
 
-            #Sets the damage and bullets with the class func
-            damage, bullets = player_weapon.shoot()
+        def encounter(name):
 
-            enemy.health -= damage
+            #picks a random message and a random name for the encounter.
+            #example output:
+            #You see a Bandit in an alley way
 
-            #Sets things health to zero if its negative for nicer visuals
-            if enemy.health < 0:
-                enemy.health = 0
+            random_names = ("Bandit", "Bastard", "Robber", "Thug", "Brute", "Foe", "Savage")
 
 
-            print(f"You shot {bullets} rounds for a total of {damage} damage")
-            print(f"The {enemy.name} now has {enemy.health} health remaining")
+            if not name:
+                name = random.choice(random_names)
+
+            random_text = (
+
+                f"You see a {name} in an alley way",
+                f"You spot a {name} hanging around",
+                f"You see the head  of a {name} poking out of a bush",
+                f"A {name} looks at you for a little to long",
+                f"You notice a {name} mugging someone",
+                f"You see a {name} looking for trouble",
+            )
+
+            text = random.choice(random_text)
+            print(random_text)
+
+            #Lets you choose wether to fight or sneak past,
+            # but  if you sneak past there's a chance of you getting caught and then the enemy gets to shoot first
+
+            while True:
+
+                print("You can try to sneak past. Or fight!")
+                choice = str(input("Press 1 to sneak past, or 2 to Fight!\n> ")).strip()
+
+                if choice == "1":
+                    print("You chose to sneak past")
+                    break
+
+                elif choice == "2":
+                    print("You chose to Fight!")
+                    break
+
+                else:
+                    print("You must pick a valid option")
+
+
+            #Just sends you to the battle function depending on what you pressed, and also makes the chance of getting caught
+
+            if choice == "1":
+
+                if random.randint(0,1):
+
+                    print(f"You managed to successfully sneak past the {name}")
+
+                else:
+                    print("The {name} caught you!!!")
+
+                    battle(False,name)
+
+            if choice == "2":
+
+                battle(True,name)
+
+
+
+
+
+        def slow_text(text):
+
+            #Just makes the text come out a bit slower, and makes it so punctuation has a little more delay
+
+
+            for letter in str(text):
+                print(letter,end = '',flush= True)
+
+                if letter in (",",".","!",":","?"):
+                    time.sleep(0.1)
+
+                else: time.sleep(0.03)
+
             print()
 
 
-        do_you_shoot_first = True
 
-        if enemy.health <= 0:
-            break
 
 
-        #picks a random weapon out of the enemys inventory
-        op_weapon = random.choice(enemy.weapons)
 
-        #just does the same thing as before but for the enemy.
+        #Makes the player class, so its easy to interact with the player.
 
+        class Player:
+            def __init__(self, name, health,inv):
+                self.name = name
+                self.health = health
+                self.inv = inv
 
 
-        damage, bullets = op_weapon.shoot()
+        #Allows a lot of weapons to be made cleanly.
+        class Weapon:
 
-        player.health -= damage
 
-        if player.health <0:
-            player.health = 0
+            #Makes a random pickup verb so its different each time
+            pick_up_verbs = ["grab", "take", "lift", "secure", "shoulder"]
 
-        print(f"The {enemy.name} shot {bullets} into you for a total of {damage} damage")
-        print(f"You are now on {player.health} health")
-        print()
+            def __init__(self, name,damage,clip_cap,accuracy,fire_rate,bullet,value,pickup_message):
+                self.name = name
+                self.damage = damage
+                self.clip_cap = clip_cap
+                self.accuracy = accuracy/100
+                self.fire_rate = fire_rate
+                self.bullet = bullet
+                self.value = value
+                self.pickup_message = pickup_message
 
 
 
 
-    print("Somebody died")
+            def shoot(self):
 
-def encounter(name):
+                #Calculate how much damage is done per turn.
+                #and add a bit of random variation so you might get slighty diffrent damage and stuff each time
 
-    #picks a random message and a random name for the encounter.
-    #example output:
-    #You see a Bandit in an alley way
 
-    random_names = ("Bandit", "Bastard", "Robber", "Thug", "Brute", "Foe", "Savage")
+                variation = random.uniform(0.8, 1.2)
 
+                bullets = (self.fire_rate / 60)*self.accuracy
 
-    if not name:
-        name = random.choice(random_names)
+                bullets = round(bullets*variation)
 
-    random_text = (
+                variation = random.uniform(0.9, 1.1)
+                total_damage = round(variation * bullets * self.damage)
 
-        f"You see a {name} in an alley way",
-        f"You spot a {name} hanging around",
-        f"You see the head  of a {name} poking out of a bush",
-        f"A {name} looks at you for a little to long",
-        f"You notice a {name} mugging someone",
-        f"You see a {name} looking for trouble",
-    )
+                return total_damage ,bullets
 
-    text = random.choice(random_text)
-    slow_text(random_text)
 
-    #Lets you choose wether to fight or sneak past,
-    # but  if you sneak past there's a chance of you getting caught and then the enemy gets to shoot first
+            def pick_up(self):
 
-    while True:
+                #Makes the random pickup thing.
+                #example output:
+                #You lift the AK-47. Chambered in 7.62 blah blah blah
 
-        print("You can try to sneak past. Or fight!")
-        choice = str(input("Press 1 to sneak past, or 2 to Fight!\n> ")).strip()
+                verb = random.choice(self.pick_up_verbs)
 
-        if choice == "1":
-            print("You chose to sneak past")
-            break
 
-        elif choice == "2":
-            print("You chose to Fight!")
-            break
+                print(self.pickup_message.format(verb= verb))
 
-        else:
-            print("You must pick a valid option")
+        #A class for the enemy.
+        #I might not end up using it
 
+        class Enemy:
+            def __init__(self,name,health,weapons):
+                self.name = name
+                self.health = health
+                self.weapons = weapons
 
-    #Just sends you to the battle function depending on what you pressed, and also makes the chance of getting caught
+            def attack(self):
+                pass
 
-    if choice == "1":
+        #A class for items, which can be randomly picked up at points and can be sold for money.
 
-        if random.randint(0,1):
+        class Item:
+            def __init__(self,name,value):
+                self.name = name
+                self.value = value
 
-            slow_text(f"You managed to successfully sneak past the {name}")
 
-        else:
-            print(f"The {name} caught you!!!")
+        #Makes the player with the player class
+        player = Player("Player",1000,[])
 
-            battle(False,name)
 
-    if choice == "2":
 
-        battle(True,name)
+        all_weapons = []
 
+        #Sets up all the weapons, each one has a name, damage and so on.
+        #The {verb} in the pick up message gets replaced randomly inside the class each time guns are picked up
 
+        weapon_data = [
 
+            ("AK-47",40,30,70,650,"assault",500,"You {verb} the AK-47, Chambered in 7.62. It feels heavy in your hands. Maybe too heavy"),
+            ("AK-12",34,30,80,750,"assault",700,"You {verb} the AK-12. You know this gun. It's like the '47', but more balanced. In theory"),
+            ("Fal 50.0",49,20,75,400,"assault",1000,"You {verb} the FAl 50.0. It takes 7.62x51mm Nato rounds. A Beast, hopefully"),
+            ("AUG A3",32,30,90,850,"assault",1400,"You {verb} the AUG A3. The compact bullpup package, boasting supreme accuracy. "),
+            ("P90",24,50,60,1100,"smg",1200,"Cold polymer meets your grip as you {verb} the P90. You see the 50 round box mag. Its ready to take on a hoard"),
+            ("G11",15,33,65,2100,"assault",2600,"As you {verb} the G11, you feel the spirit of West Germany. Experimental, desperate, and ahead of its time."),
 
 
-def slow_text(text):
+        ]
 
-    #Just makes the text come out a bit slower, and makes it so punctuation has a little more delay
+        #Simply adds all the guns as classes to a list
 
+        for i in weapon_data:
 
-    for letter in str(text):
-        print(letter,end = '',flush= True)
+            all_weapons.append(Weapon(*i))
 
-        if letter in (",",".","!",":","?"):
-            time.sleep(0.1)
 
-        else: time.sleep(0.03)
 
+        def main():
+            print("Welcome to the game of something hopefully cool")
 
 
+        #Just some testing stuff. temporary
+        player.inv.append(all_weapons[0])
 
+        battle(True,"Bastard")
 
+        main()
 
-
-
-#Makes the player class, so its easy to interact with the player.
-
-class Player:
-    def __init__(self, name, health,inv):
-        self.name = name
-        self.health = health
-        self.inv = inv
-
-
-#Allows a lot of weapons to be made cleanly.
-class Weapon:
-
-
-    #Makes a random pickup verb so its different each time
-    pick_up_verbs = ["grab", "take", "lift", "secure", "shoulder"]
-
-    def __init__(self, name,damage,clip_cap,accuracy,fire_rate,bullet,value,pickup_message):
-        self.name = name
-        self.damage = damage
-        self.clip_cap = clip_cap
-        self.accuracy = accuracy
-        self.fire_rate = fire_rate
-        self.bullet = bullet
-        self.value = value
-        self.pickup_message = pickup_message
-
-
-
-
-    def shoot(self):
-
-        #Calculate how much damage is done per turn.
-        #and add a bit of random variation so you might get slighty diffrent damage and stuff each time
-        variation = random.uniform(0.8, 1.2)
-        bullets = round((self.fire_rate/60)*variation)
-
-        variation = random.uniform(0.9, 1.1)
-        total_damage = round(variation * bullets * self.damage)
-
-        return total_damage,bullets
-
-
-    def pick_up(self):
-
-        #Makes the random pickup thing.
-        #example output:
-        #You lift the AK-47. Chambered in 7.62 blah blah blah
-
-        verb = random.choice(self.pick_up_verbs)
-
-
-        slow_text(self.pickup_message.format(verb= verb))
-
-#A class for the enemy.
-#I might not end up using it
-
-class Enemy:
-    def __init__(self,name,health,weapons):
-        self.name = name
-        self.health = health
-        self.weapons = weapons
-
-    def attack(self):
-        pass
-
-#A class for items, which can be randomly picked up at points and can be sold for money.
-
-class Item:
-    def __init__(self,name,value):
-        self.name = name
-        self.value = value
-
-
-#Makes the player with the player class
-player = Player("Player",1000,[])
-
-
-
-all_weapons = []
-
-#Sets up all the weapons, each one has a name, damage and so on.
-#The {verb} in the pick up message gets replaced randomly inside the class each time guns are picked up
-
-weapon_data = [
-
-    ("AK-47",40,30,70,650,"assault",500,"You {verb} the AK-47, Chambered in 7.62. It feels heavy in your hands. Maybe too heavy"),
-    ("AK-12",34,30,80,750,"assault",700,"You {verb} the AK-12. You know this gun. It's like the '47', but more balanced. In theory"),
-    ("Fal 50.0",49,20,75,400,"assault",1000,"You {verb} the FAl 50.0. It takes 7.62x51mm Nato rounds. A Beast, hopefully"),
-    ("AUG A3",32,30,90,850,"assault",1400,"You {verb} the AUG A3. The compact bullpup package, boasting supreme accuracy. "),
-    ("P90",24,50,60,1100,"smg",1200,"Cold polymer meets your grip as you {verb} the P90. You see the 50 round box mag. Its ready to take on a hoard"),
-    ("G11",15,33,65,2100,"assault",2600,"As you {verb} the G11, you feel the spirit of West Germany. Experimental, desperate, and ahead of its time."),
-
-
-]
-
-#Simply adds all the guns as classes to a list
-
-for i in weapon_data:
-
-    all_weapons.append(Weapon(*i))
-
-
-
-def main():
-    print("Welcome to the game of something hopefully cool")
-
-
-#Just some testing stuff. temporaray
-player.inv.append(all_weapons[0])
-
-battle(True,"Bastard")
-
-main()
+    except KeyboardInterrupt:
+        print("Game Restarted")
